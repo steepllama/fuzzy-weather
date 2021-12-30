9d4270116482192202e950392479942049f42724
steepllama <steepllama@gmail.com>
2021-12-30 14:55:39 -0600

    deleted all existing files and updated newer files with a whole new layout. Currently upddated the index html file and the header css file

---
+++ assets/home.js
@@ -0,0 +1,13 @@
+// variable elements
+var cityEl = document.querySelector(".class");
+var mondayEl = document.querySelector(".monday");
+var mondayEl = document.querySelector(".tuesday");
+var mondayEl = document.querySelector(".wednesday");
+var mondayEl = document.querySelector(".thursday");
+var mondayEl = document.querySelector(".friday");
+
+// api Key
+
+// api URL
+
+// variable functions
