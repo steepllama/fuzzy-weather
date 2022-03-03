6700f17ceda935ea95c92cf3bd38c5b3a01cab37
steepllama <steepllama@gmail.com>
2022-03-03 12:44:15 -0600

    cleaned out html and javascript to re do the whole thing over again

--- assets/home.js
+++ assets/home.js
@@ -1,13 +1,1 @@
-// variable elements
-var cityEl = document.querySelector(".class");
-var mondayEl = document.querySelector(".monday");
-var mondayEl = document.querySelector(".tuesday");
-var mondayEl = document.querySelector(".wednesday");
-var mondayEl = document.querySelector(".thursday");
-var mondayEl = document.querySelector(".friday");
-
-// api Key
-
-// api URL
-
-// variable functions
+let 
